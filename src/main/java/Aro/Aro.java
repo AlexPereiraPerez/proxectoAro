953729eb316e5c3fddf0ca910f74d72c914a6857
Alex <a20alexandrepp@iessanclemente.net>
2022-03-04 17:11:34 +0100

    Analizador de javadoc aplicado e xa non avisade que falta ningún comentario

--- src/main/java/Aro/Aro.java
+++ src/main/java/Aro/Aro.java
@@ -7,39 +7,72 @@ package Aro;
 public class Aro {
 
     private static final double LIMITERADIO = 0.0;
+
+    /**
+     * Obter o valor da constente MINIMO
+     */
     public static final double MINIMO = LIMITERADIO;
  
     private int coordenadaX;
     private int coordenadaY;
     private double radio;
 
+    /**
+     * Constructor da clase Aro
+     */
     public Aro() {
     }
 
+    /**
+     * Constructor
+     * @param valorX
+     * @param valorY
+     * @param valorRadio
+     */
     public Aro(int valorX, int valorY, double valorRadio) {
         coordenadaX = valorX;
         coordenadaY = valorY;
         setRadio(valorRadio);
     }
 
-    
+    /**
+     *
+     * @return Diametro
+     */
     public double obterDiametro() {
         return getRadio() * 2;
     }
 
+    /**
+     *
+     * @return Circunrerencia
+     */
     public double obterCircunferencia() {
         return Math.PI * obterDiametro();
     }
 
+    /**
+     *
+     * @return Superficie
+     */
     public double obterSuperfice() {
         return Math.PI * getRadio() * getRadio();
     }
 
+    /**
+     *
+     * @return coordenadas.
+     */
     @Override
     public String toString() {
         return "Centro = [" + getCoordenadaX() + "," + getCoordenadaY() + "]; Radio = " + getRadio();
     }
 
+    /**
+     *
+     * @param trasladarX
+     * @param trasladarY
+     */
     public void trasladarCentro(int trasladarX, int trasladarY){
         setCoordenadaX(getCoordenadaX() + trasladarX);
         setCoordenadaY(getCoordenadaY() + trasladarY);
--- src/main/java/Aro/Main.java
+++ src/main/java/Aro/Main.java
@@ -7,6 +7,11 @@ import java.text.DecimalFormat;
  * @author Álex Pereira
  */
 public class Main {
+
+    /**
+     * Clase Main
+     * @param args
+     */
     public static void main(String[] args) {
         Aro Aro = new Aro(37,43,2.5);
         String saida =
